dba4ff79784a24c0b08ffc8dd11499b8a617ef73
Asuki Kono <asukiaaa@gmail.com>
2016-12-09 22:11:38 +0900

    Initial commit

---
+++ PCBHeater.ino
@@ -0,0 +1,42 @@
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+
+#define DO   3
+#define CS   4
+#define CLK  5
+#define RELAY_PIN 6
+
+#define HEATER_OFF_TEMP 240
+
+Adafruit_MAX31855 thermocouple(CLK, CS, DO);
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(115200);
+
+  Serial.println("start PCB heater");
+  delay(500);
+}
+
+void loop() {
+  Serial.print("Internal Temp = ");
+  Serial.println(thermocouple.readInternal());
+
+  double c = thermocouple.readCelsius();
+  if (isnan(c)) {
+    Serial.println("Something wrong with thermocouple!");
+  } else {
+    Serial.print("C = ");
+    Serial.println(c);
+  }
+
+  if ( c > HEATER_OFF_TEMP ) {
+    digitalWrite(RELAY_PIN, LOW);
+    Serial.println("heater off");
+  } else {
+    digitalWrite(RELAY_PIN, HIGH);
+    Serial.println("heater on");
+  }
+
+   delay(1000);
+}
